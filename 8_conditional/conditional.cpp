63c4cba3e802642712eb3ca3f524445938bed5ad
dervalinor <dervalinor@protonmail.com>
2022-07-11 18:31:56 -0500

    Conditional and logical operators

---
+++ 8_conditional/conditional.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main() {
+  //conditional: it use to make decision by evaluating conditional
+  /*
+   We’ll be covering the following concepts:
+
+  if, else if, and else statements
+  switch statements
+  Relational operators
+  Logical operators
+
+if (condition) {
+
+  some code
+
+}
+
+To have a condition, we need relational operators:
+
+== equal to
+!= not equal to
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+  */
+  // Create a number that's 0 or 1
+  
+  srand (time(NULL));
+  int	coin = rand() % 2;
+  
+  // If number is 0: Heads
+  // If it is not 0: Tails
+  
+  if (coin == 0) {
+  
+    std::cout << "Heads\n";
+  
+  }
+	else {
+	
+    std::cout << "Tails\n";
+  
+  }
+  
+}
