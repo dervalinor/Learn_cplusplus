aa7bddd08e281d0d9810657385c5ba412e6d8ebe
dervalinor <dervalinor@protonmail.com>
2022-07-21 12:39:05 -0500

    Challenge of operator logical

---
+++ 9_operator_logical/review.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main () {
+/*
+Review
+Awesome! In this mini-lesson, we’ve added more operators to our toolbox:
+
+&&: the and logical operator
+||: the or logical operator
+!: the not logical operator
+
+1.
+Optional: Let’s try a challenging problem that will put all your brain cells to the test.
+
+Write a leap_year.cpp program that:
+
+Takes a year as input.
+Checks to see if the year is a four-digit number.
+Displays whether or not the year falls on a leap year.
+There are 3 criteria that must be taken into account to identify a leap year:
+
+If the year can be evenly divided by 4 then it is a leap year, however…
+If that year can be evenly divided by 100, and it is not evenly divided by 400, then it is NOT a leap year.
+If that year is evenly divisible by 400, then it is a leap year.
+Take some time to research and brainstorm before starting to write the code!
+
+*/
+
+  int year;
+
+  std::cout << "Year: "; std::cin >> year;
+
+  if(year%4 == 0 || year%400 == 0){
+    std::cout << "Leap year. \n";
+  } else if (year%100 && year%400 != 0) {
+    std::cout << "Not a leap year. \n";
+  } else {
+    std::cout << "Not a leap year. \n";
+  }
+
+}
